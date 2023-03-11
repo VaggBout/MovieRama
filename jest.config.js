d560275d7152c87db8db53f480715371a895bd1e
Vaggelis Boutas <vaggbout@hotmail.com>
2023-03-11 14:12:12 +0200

    add tests config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    coverageReporters: ["html", "lcov", "text"],
+    coverageDirectory: "<rootDir>/coverage",
+    transform: {
+        "^.+\\.ts": "ts-jest",
+    },
+    testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+    modulePathIgnorePatterns: ["<rootDir>/dist"],
+};
