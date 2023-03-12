425f8186172c80b12533d4577773cc427ea10539
Vaggelis Boutas <vaggbout@hotmail.com>
2023-03-12 20:12:00 +0200

    remove console

--- src/controllers/homepage.ts
+++ src/controllers/homepage.ts
@@ -5,7 +5,6 @@ export async function get(_req: Request, res: Response): Promise<void> {
     try {
         const userId = res.locals.user ? res.locals.user.id : null;
         const result = await MovieService.getMoviesPage("DESC", 5, 0, userId);
-        console.log(result);
         res.locals.movies = result.data ? result.data : [];
         return res.render("homepage");
     } catch (error) {
