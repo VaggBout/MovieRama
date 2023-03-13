a7424328d1dc5d248ee190cf5f037ae4c8d380d1
Vaggelis Boutas <vaggbout@hotmail.com>
2023-03-13 19:58:53 +0200

    add movie controller tests

--- __tests__/controllers/movie.controller.test.ts
+++ __tests__/controllers/movie.controller.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, jest, test } from "@jest/globals";
+import { beforeEach, describe, expect, jest, test } from "@jest/globals";
 import * as MovieController from "../../src/controllers/api/movie";
 import { Movie } from "../../src/models/movie";
 import * as MovieService from "../../src/services/movie";
@@ -6,6 +6,11 @@ import express from "express";
 import { DateTime } from "luxon";
 
 describe("Movie controller", () => {
+    beforeEach(() => {
+        jest.restoreAllMocks();
+        jest.clearAllMocks();
+    });
+
     describe("create", () => {
         test("should respond with movie object when create is success", async () => {
             const mockCreate = jest
@@ -40,8 +45,6 @@ describe("Movie controller", () => {
 
             await MovieController.post(req, res);
             expect(mockCreate).toHaveBeenCalledTimes(1);
-
-            mockCreate.mockClear();
         });
 
         test("should respond with 400 when service returns error", async () => {
@@ -72,8 +75,6 @@ describe("Movie controller", () => {
             await MovieController.post(req, res);
             expect(mockCreate).toHaveBeenCalledTimes(1);
             expect(res.statusCode).toBe(400);
-
-            mockCreate.mockClear();
         });
 
         test("should respond with 500 when service throws error", async () => {
@@ -104,8 +105,99 @@ describe("Movie controller", () => {
             await MovieController.post(req, res);
             expect(mockCreate).toHaveBeenCalledTimes(1);
             expect(res.statusCode).toBe(500);
+        });
+    });
 
-            mockCreate.mockClear();
+    describe("get", () => {
+        test("returns rendered html and totalMovies", async () => {
+            const mockGetMoviesPage = jest
+                .spyOn(MovieService, "getMoviesPage")
+                .mockImplementationOnce(() =>
+                    Promise.resolve({
+                        data: {
+                            movies: [
+                                {
+                                    id: 1,
+                                    title: "test",
+                                    description: "test",
+                                    daysElapsed: "test",
+                                    userId: 1,
+                                    userName: "test",
+                                    likes: 1,
+                                    hates: 1,
+                                    vote: true,
+                                },
+                            ],
+                            totalMovies: 1,
+                        },
+                    })
+                );
+
+            const req = {
+                query: {
+                    order: "likes",
+                    sort: "ASC",
+                    page: 2,
+                },
+                send: jest.fn(),
+                app: {
+                    render: jest
+                        .fn()
+                        .mockImplementationOnce((_file, _data, cb: any) =>
+                            cb(null, "test html")
+                        ),
+                },
+            } as unknown as express.Request;
+
+            const res = {
+                locals: {
+                    user: { id: 1 },
+                },
+                send: jest.fn(),
+            } as unknown as express.Response;
+
+            await MovieController.get(req, res);
+            expect(mockGetMoviesPage).toHaveBeenCalledTimes(1);
+            expect(mockGetMoviesPage).toHaveBeenCalledWith(
+                "ASC",
+                5,
+                10,
+                "likes",
+                1,
+                null
+            );
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.send).toHaveBeenCalledWith({
+                html: "test html",
+                data: { totalMovies: 1 },
+            });
+            expect(req.app.render).toHaveBeenCalledTimes(1);
+        });
+
+        test("respond with 500 when movies service throws", async () => {
+            const mockGetMoviesPage = jest
+                .spyOn(MovieService, "getMoviesPage")
+                .mockImplementationOnce(() => {
+                    throw new Error("test");
+                });
+
+            const req = {
+                query: {},
+                send: jest.fn(),
+            } as unknown as express.Request;
+
+            const res = {
+                statusCode: 0,
+                locals: {
+                    user: { id: 1 },
+                },
+                send: jest.fn(),
+            } as unknown as express.Response;
+
+            await MovieController.get(req, res);
+            expect(mockGetMoviesPage).toHaveBeenCalledTimes(1);
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.statusCode).toBe(500);
         });
     });
 });
